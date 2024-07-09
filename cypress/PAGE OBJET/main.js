61f4f109d849fcd85c8c04792c5cfc47accaa1f5
Ana Cano <anacanobaena@gmail.com>
2024-07-09 18:34:27 +0200

    corrección POM

--- cypress/PAGE OBJET/main.js
+++ cypress/PAGE OBJET/main.js
@@ -1,7 +1,5 @@
 class main {
-   texto1 = "tarea1"
-   texto2 = "tarea2"
-   texto3 = "tarea3"
+   
 
     navegar() {
        return cy.visit('https://todomvc.com/examples/react/dist/')
@@ -9,11 +7,11 @@ class main {
     seleccionarElemento() {
         return cy.get('[data-testid="text-input"]').click()
      }
-    escribirTexto() {
-        return cy.get('[data-testid="text-input"]').type(this.texto1 + "{enter}")
+    escribirTexto(texto) {
+        return cy.get('[data-testid="text-input"]').type(texto + "{enter}")
      }
-    VerificarTexto() {
-        return cy.get('[data-testid="todo-item-label"]').contains(this.texto1)
+    VerificarTexto(texto) {
+        return cy.get('[data-testid="todo-item-label"]').contains(texto)
      }
     seleccionarSeccion() {
         return cy.get('body').click()
